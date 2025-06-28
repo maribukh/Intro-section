bdb7a261bf6ffa17c2fc5741a03ea21aa81d0502
shota777-dotcom <mariebukhaidze@gmail.com>
2025-06-28 13:29:26 +0400

    first commit

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import "../Button/Button.css";
+
+export default function Button(props) {
+  return (
+    <>
+      <button>{props.text}</button>
+    </>
+  );
+}
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import logo from "../../assets/logo.svg";
+import arrowDown from "../../assets/images/icon-arrow-down.svg";
+import Button from "../Button/Button";
+import "../Header/Header.css";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        {" "}
+        <div className="left">
+          {" "}
+          <img src={logo} alt="loading..." />
+          <nav>
+            <ul>
+              <li>
+                Features <img src={arrowDown} alt="loading..." />
+              </li>
+              <li>
+                Company <img src={arrowDown} alt="loading..." />
+              </li>
+              <li>Careers</li>
+              <li>About</li>
+            </ul>
+          </nav>
+        </div>
+        <div className="right">
+          <Button text="Login" />
+          <Button text="Register" />
+        </div>
+      </header>
+    </>
+  );
+}
+
+Header();
